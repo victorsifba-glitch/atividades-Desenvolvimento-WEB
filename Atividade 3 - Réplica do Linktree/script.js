cc3c2713d198311547e0e5b59077677dd8ea3e3a
VictorS-IFBA <victor.s.ifba@gmail.com>
2026-07-07 18:10:49 -0300

    Add files via upload

--- Atividade 3 - Réplica do Linktree/script.js
+++ Atividade 3 - Réplica do Linktree/script.js
@@ -1,34 +1,35 @@
-const links_principais = [
+const linksPrincipais = [
     { label: "Instagram", url: "https://www.instagram.com/victorsantossilva2006/" },
     { label: "YouTube", url: "https://www.youtube.com/@megav6142" },
     { label: "GitHub", url: "https://github.com/victorsifba-glitch" },
     { label: "Contato", url: "https://web.whatsapp.com" }
 ];
 
-const redes_sociais = [
+const redesSociais = [
     { nome: "TikTok", url: "https://www.tiktok.com", img: "tiktok.png" },
     { nome: "X", url: "https://x.com", img: "x.webp" },
     { nome: "Facebook", url: "https://www.facebook.com", img: "facebook.webp" },
     { nome: "Bluesky", url: "https://bsky.app", img: "bluesky.png" }
 ];
-async function renderizarLinks() {
-    const containerPrincipal = document.getElementById("links_principais");
-    const containerRedes = document.getElementById("redes_sociais");
-    const resposta = await fetch('api.php');
-    const dados = await resposta.json();
 
-    dados.links.forEach(link => {
+function renderizarLinks() {
+    const containerPrincipal = document.getElementById("main-links");
+    const containerRedes = document.getElementById("social-links");
+
+    linksPrincipais.forEach(link => {
         const elementoA = document.createElement("a");
         elementoA.href = link.url;
         elementoA.textContent = link.label;
         elementoA.target = "_blank";
+        elementoA.rel = "noopener noreferrer";
         containerPrincipal.appendChild(elementoA);
     });
 
-    dados.redes.forEach(link => {
+    redesSociais.forEach(link => {
         const elementoA = document.createElement("a");
         elementoA.href = link.url;
         elementoA.target = "_blank";
+        elementoA.rel = "noopener noreferrer";
 
         const elementoImg = document.createElement("img");
         elementoImg.src = link.img;
@@ -38,4 +39,6 @@ async function renderizarLinks() {
         containerRedes.appendChild(elementoA);
     });
 }
+
+// Executa a função assim que a estrutura da página (DOM) estiver totalmente carregada
 window.addEventListener("DOMContentLoaded", renderizarLinks);
